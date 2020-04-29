bcf23bfcab280033cae6250df5f8b5d5a72a6d25
Hanna Miller <hanna.miller@students.dominican.edu>
2020-04-28 22:22:18 -0700

    Initial Commit

---
+++ CSFinal2020/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CSFinal2020
+//
+//  Created by Hanna Miller on 4/28/20.
+//  Copyright © 2020 Hanna Miller. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
